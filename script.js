126464cfca2746bfbe107b91419e33e70274d3a1
Jason Hensley <jasonhensley1989@gmail.com>
2023-05-03 15:42:39 -0500

    users and json

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ const getText = () => {
     });
 }
 
+const getUsers = () => {
+    fetch('users.json')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '<h2>Users</h2>';
+        data.forEach((user) => {
+            output += `
+            <ul>
+                <li>Name: ${user.name}</li>
+                <li>Email: ${user.email}</li>
+                <li>${user.id}</li>
+            </ul>
+            `;
+        })
+        document.getElementById('output').innerHTML = output;
+    })
+}
+
 document.getElementById('getText').addEventListener("click", getText);
+document.getElementById('getUsers').addEventListener("click", getUsers);
 
 
 
