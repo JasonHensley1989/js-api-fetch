9c61f361ba32f961ce189eb198964dbf33b321e2
Jason Hensley <jasonhensley1989@gmail.com>
2023-05-15 15:43:37 -0500

    scope  naming convention

--- scope.js
+++ scope.js
@@ -35,13 +35,29 @@ test2();
 // function scope and block using var
 const test3 = () => {
     var d = 2;
-
+    let f = 4;
     if(true) {
         var e = 3;   
+        let g = 5;
     }
     // e can be logged here because of the scope that var uses
-    // this is what const and let rectify
+    // this is what const and let rectify if its changed to let
+    // it has block scope only again
+  console.log(d, f);
   console.log(e);
 }
 
 test3();
+
+// this test shows why non repetitive variable names are necessary
+
+const test4 = () => {
+    const a = 9;
+    if(true) {
+        const a = 10;
+        console.log(a);
+    }
+    console.log(a);
+}
+
+test4();
